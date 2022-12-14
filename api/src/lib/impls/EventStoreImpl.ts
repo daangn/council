7d0b51edc5b20d5d6c8ae05d59ba51fb1950f411
Hyeseong Kim <tim@daangn.com>
2022-12-14 22:35:41 +0900

    added CouncilEventSnapshot

--- api/src/lib/impls/EventStoreImpl.ts
+++ api/src/lib/impls/EventStoreImpl.ts
@@ -7,21 +7,43 @@ interface CreateEventStoreImpl {
 }
 
 export const createEventStoreImpl: CreateEventStoreImpl = ({ db }) => {
+  const snapshotPer = 20;
+  const snapshotLifeTracker = new Map<string, number>();
+
   return {
     async persist(aggregate) {
+      const streamId = aggregate.value.id;
+      const sequence = aggregate.value.seq;
       const events = aggregate.value.events;
       if (events.length === 0) {
         return aggregate;
       }
 
+      let life = snapshotLifeTracker.get(streamId) ?? 0;
+      life += events.length;
+
       await db.councilEvent.createMany({
         data: events.map((event, i) => ({
-          stream_id: aggregate.value.id,
-          sequence: aggregate.value.seq + i,
+          stream_id: streamId,
+          sequence: sequence + i + 1,
           date: new Date(event.value.date),
           data: event,
         })),
       });
+
+      if (life >= snapshotPer) {
+        await db.councilSnapshot.create({
+          data: {
+            aggregate_name: aggregate.tag,
+            stream_id: streamId,
+            sequence: sequence + events.length,
+            data: aggregate.value,
+          },
+        });
+        snapshotLifeTracker.set(streamId, 0);
+      } else {
+        snapshotLifeTracker.set(streamId, life);
+      }
 
       return {
         ...aggregate,
